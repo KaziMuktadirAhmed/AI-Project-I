70db7389d60f573f62ece46f1b1b8369657b7fd0
Kazi Muktadir Ahmed <bsse1111@iit.du.ac.bd>
2022-08-12 20:39:01 +0600

    Update Game.java

--- 2-player-test/Game/Game.java
+++ 2-player-test/Game/Game.java
@@ -3,19 +3,21 @@ package Game;
 public class Game {
     private int board[][] = new int[6][7];
 
+    private int hasWinner () {
+        int winner = -1;
+        
+        return winner;
+    }
+
     public void showBoard() {
         String output = "";
-
         for (int i=0; i<6; ++i) {
             String line = "";
-
             for (int j=0; j<7; ++j) {
                 line += (board[i][j] + " ");
             }
-
             output += (line + "\n");
         }
-
         System.out.println(output);
     }
 }
