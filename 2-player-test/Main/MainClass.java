74197e5ae9076a797b9c922f8e8a873d45261cd3
Kazi Muktadir Ahmed <bsse1111@iit.du.ac.bd>
2022-08-14 23:27:19 +0600

    Update MainClass.java

--- 2-player-test/Main/MainClass.java
+++ 2-player-test/Main/MainClass.java
@@ -6,19 +6,15 @@ public class MainClass {
     public static void main(String[] args) {
 //        System.out.println("hello world");
         test();
+        driver();
+    }
+
+    public static void driver() {
+        Game game = new Game();
+        game.play();
     }
 
     public static void test() {
-        Game game = new Game();
-//        game.showBoard();
-        game.play();
-//        int count = 1;
-//        for (int i=0; i<20; i++) {
-//            System.out.print(count);
-//            if(count == 1) count = 2;
-//            else if (count == 2) {
-//                count = 1;
-//            }
-//        }
+
     }
 }
