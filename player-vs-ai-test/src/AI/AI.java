a9038bde819fbe7a7c603f0303924dd8e7da4589
Kazi Muktadir Ahmed <bsse1111@iit.du.ac.bd>
2022-08-25 23:02:26 +0600

    fixed the single tile evaluation algo

--- player-vs-ai-test/src/AI/AI.java
+++ player-vs-ai-test/src/AI/AI.java
@@ -131,7 +131,7 @@ public class AI {
                 }
             }
         }
-        return (128 + sum) * (max_depth - level);
+        return (138 + sum) * (max_depth - level);
     }
 
     public void printLastGeneratedGameTree(GameTree tree) throws FileNotFoundException {
